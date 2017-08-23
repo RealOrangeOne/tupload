9fb7ae4bac496d81825e3af3d61b76b0bdad0b9e
Jake Howard <git@theorangeone.net>
2017-08-23 14:33:58 +0100

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="tupload",
     version="1.0.0",
     install_requires=[],
-    include_package_data=True,
     entry_points="""
         [console_scripts]
         upload=tupload.upload:cli
